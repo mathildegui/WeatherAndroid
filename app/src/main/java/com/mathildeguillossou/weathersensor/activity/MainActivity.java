81d0a12fc0f2f4cbaad452301df899e65f735d61
mathildegui <mathilde.guillossou@gmail.com>
2015-12-05 13:01:11 +0100

    add color

--- app/src/main/java/com/mathildeguillossou/weathersensor/activity/MainActivity.java
+++ app/src/main/java/com/mathildeguillossou/weathersensor/activity/MainActivity.java
@@ -7,8 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.mathildeguillossou.weathersensor.api.ApiClassInterface;
 import com.mathildeguillossou.weathersensor.R;
@@ -44,28 +42,6 @@ public class MainActivity extends AppCompatActivity implements ApiClassInterface
     private void getWeathers() {
         ApiManager apiManager = new ApiManager(this);
         apiManager.loadW();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
